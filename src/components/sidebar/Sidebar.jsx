f3ee4056507b992e95e182a4edb146bca6c04cc5
Rach Syahputra <rach.syahputra@gmail.com>
2024-02-16 11:30:31 +0800

    created sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import "./sidebar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBolt, faCompactDisc, faMicrophone, faMusic } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+      <div className="logo">
+        LIRIKA
+      </div>
+
+      <div className="menu">
+        <h5>MENU</h5>
+        <ul>
+          <li><span className='icon'><FontAwesomeIcon icon={faBolt} /></span> Explore</li>
+          <li><span className='icon'><FontAwesomeIcon icon={faMusic} /></span> Genre</li>
+          <li><span className='icon'><FontAwesomeIcon icon={faCompactDisc} /></span> Album</li>
+          <li><span className='icon'><FontAwesomeIcon icon={faMicrophone} /></span> Artist</li>
+        </ul>
+      </div>
+
+      <div className="footer">
+        All Right Reserved 2024
+      </div>
+    </div>
   )
 }
 
