87ab993ec3f3cc878b992c621822be4bf110ec0e
Rach Syahputra <rach.syahputra@gmail.com>
2024-02-16 09:53:53 +0800

    added main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearchengin } from '@fortawesome/free-brands-svg-icons'
+import MusicPlayer from './components/musicPlayer/MusicPlayer'
+import Main from './components/main/Main'
+import Sidebar from './components/sidebar/Sidebar'
+
 
 function App() {
 
 
   return (
     <div className="container">
-
+      <Sidebar />
+      <Main />
+      <MusicPlayer />
     </div>
   )
 }
