dd42732d3820fbe3e3e31a6856676ec0cefa6538
Rach Syahputra <rach.syahputra@gmail.com>
2024-02-18 07:29:05 +0800

    updated style

--- src/components/topSong/TopSong.jsx
+++ src/components/topSong/TopSong.jsx
@@ -1,95 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./topSong.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const TopSong = () => {
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/songs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setSongs(data)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }, [])
+
   return (
     <div className='topSong'>
       <div className="header">
         <h3>Top Songs</h3>
         <h5>See All</h5>
       </div>
+
       <div className="songList">
-        <div className="item">
-          <div className="info">
-            <h5>01</h5>
-            <img src="https://www.spirit-of-metal.com/les%20goupes/B/Beyond%20Creation/Algorythm/Algorythm_8172.jpg" alt="" />
-            <div className="detail">
-              <h3>Song Title</h3>
-              <h5>Artist</h5>
+        {songs && songs.map((song, index) => (
+          <div className="item" key={song.id}>
+            <div className="info">
+              <h5>{index + 1}</h5>
+              <img src={song.image} alt="" />
+              <div className="detail">
+                <h3 className='title'>{song.title}</h3>
+                <h5 className='artist'>{song.artist}</h5>
+              </div>
+            </div>
+            <div className="action">
+              <h3>{song.duration}</h3>
+              <div className="playIcon">
+                <FontAwesomeIcon icon={faPlay} className='icon' />
+              </div>
+              <div className="plusIcon">
+                <FontAwesomeIcon icon={faPlus} className='icon' />
+              </div>
             </div>
           </div>
-          <div className="action">
-            <h3>03:00</h3>
-            <div className="playIcon">
-              <FontAwesomeIcon icon={faPlay} className='icon' />
-            </div>
-            <div className="plusIcon">
-              <FontAwesomeIcon icon={faPlus} className='icon' />
-            </div>
-          </div>
-        </div>
-
-        <div className="item">
-          <div className="info">
-            <h5>01</h5>
-            <img src="https://www.spirit-of-metal.com/les%20goupes/B/Beyond%20Creation/Algorythm/Algorythm_8172.jpg" alt="" />
-            <div className="detail">
-              <h3>Song Title</h3>
-              <h5>Artist</h5>
-            </div>
-          </div>
-          <div className="action">
-            <h3>03:00</h3>
-            <div className="playIcon">
-              <FontAwesomeIcon icon={faPlay} className='icon' />
-            </div>
-            <div className="plusIcon">
-              <FontAwesomeIcon icon={faPlus} className='icon' />
-            </div>
-          </div>
-        </div>
-
-        <div className="item">
-          <div className="info">
-            <h5>01</h5>
-            <img src="https://www.spirit-of-metal.com/les%20goupes/B/Beyond%20Creation/Algorythm/Algorythm_8172.jpg" alt="" />
-            <div className="detail">
-              <h3>Song Title</h3>
-              <h5>Artist</h5>
-            </div>
-          </div>
-          <div className="action">
-            <h3>03:00</h3>
-            <div className="playIcon">
-              <FontAwesomeIcon icon={faPlay} className='icon' />
-            </div>
-            <div className="plusIcon">
-              <FontAwesomeIcon icon={faPlus} className='icon' />
-            </div>
-          </div>
-        </div>
-
-        <div className="item">
-          <div className="info">
-            <h5>01</h5>
-            <img src="https://www.spirit-of-metal.com/les%20goupes/B/Beyond%20Creation/Algorythm/Algorythm_8172.jpg" alt="" />
-            <div className="detail">
-              <h3>Song Title</h3>
-              <h5>Artist</h5>
-            </div>
-          </div>
-          <div className="action">
-            <h3>03:00</h3>
-            <div className="playIcon">
-              <FontAwesomeIcon icon={faPlay} className='icon' />
-            </div>
-            <div className="plusIcon">
-              <FontAwesomeIcon icon={faPlus} className='icon' />
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   )
