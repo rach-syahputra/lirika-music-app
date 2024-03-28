2b0b6bd85393df73b49f2924af082b2a1c80bef0
Rach Syahputra <rach.syahputra@gmail.com>
2024-03-28 14:26:02 +0800

    feat(auth): Add REGISTER, LOGIN, and LOGOUT routes and controller

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import userRoute from "./routes/users.js"
 import authRoute from "./routes/auth.js"
+import cors from "cors"
+import cookieParser from "cookie-parser";
 
 const app = express()
+
+// MIDDLEWARES
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
