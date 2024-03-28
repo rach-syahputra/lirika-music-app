22aba2e75dfeb37908d82a705916464f97df0a0f
Rach Syahputra <rach.syahputra@gmail.com>
2024-03-28 13:17:17 +0800

    feat: create mysql connection and create basic route

---
+++ backend/controllers/auth.js
@@ -0,0 +1,14 @@
+export const register = (req, res) => {
+  // CHECK USER IF EXIST
+
+  // CREATE A NEW USER
+  // Hash the password
+}
+
+export const login = (req, res) => {
+
+}
+
+export const logout = (req, res) => {
+
+}
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import userRoute from "./routes/users.js"
+import authRoute from "./routes/auth.js"
 
 const app = express()
+
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
 
 app.listen(8800, () => {
   console.log("Server is working!");
