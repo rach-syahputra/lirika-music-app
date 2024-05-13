6c4bee083096dabe922c9864896260fbf68defc4
Rach Syahputra <rach.syahputra@gmail.com>
2024-05-13 16:54:31 +0800

    feat: Add custom button and song list item style

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+import './styles/index.css'
 import { AuthContextProvider } from './hooks/authContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'gray': '#919191',
+      'gray-dark': '#191920',
+      'green-light': '#1db954',
+      'white': "#f8f9fa",
+      'white-hover': "#e9ecef"
+    },
     extend: {},
   },
   plugins: [],
