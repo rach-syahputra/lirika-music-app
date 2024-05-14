a3e6ef5eddf2df58a295bd892b2df7701ce48097
Rach Syahputra <rach.syahputra@gmail.com>
2024-05-14 15:23:09 +0800

    refactor: Migrate Music Player component styling to Tailwind CSS

--- frontend/src/components/musicPlayer/MusicPlayer.jsx
+++ frontend/src/components/musicPlayer/MusicPlayer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import "./musicPlayer.css"
+import './musicPlayer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBackwardStep, faForwardStep, faPause, faPlay, faPlus, faRepeat, faShuffle, faVolumeLow } from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios'
@@ -9,11 +9,11 @@ import { setCurrentSongId } from '../../redux/reducers/currentSongSlice.js'
 import { setIsPlayedId } from '../../redux/reducers/isPlayedSlice.js'
 
 const MusicPlayer = () => {
-  const [title, setTitle] = useState("")
-  const [artist, setArtist] = useState("")
-  const [image, setImage] = useState("")
-  const [duration, setDuration] = useState("")
-  const [albumName, setAlbumName] = useState("")
+  const [title, setTitle] = useState('')
+  const [artist, setArtist] = useState('')
+  const [image, setImage] = useState('')
+  const [duration, setDuration] = useState('')
+  const [albumName, setAlbumName] = useState('')
   const songList = useSelector(state => state.songList.data)
   const currentSongId = useSelector(state => state.currentSongId.id)
   const isPlayedId = useSelector(state => state.isPlayedId.id)
@@ -25,7 +25,7 @@ const MusicPlayer = () => {
 
   const fetchData = async () => {
     try {
-      const res = await axios.get("http://localhost:8800/api/song/find/" + currentSongId)
+      const res = await axios.get('http://localhost:8800/api/song/find/' + currentSongId)
       const song = res.data
 
       setTitle(song.title)
@@ -41,49 +41,49 @@ const MusicPlayer = () => {
 
   return (
 
-    <div className='music-player'>
-      <div className="left-control">
-        <div className="img-container">
-          <img src={image ? `${image}` : 'https://f4.bcbits.com/img/a4139357031_10.jpg'} alt="" />
+    <div className='fixed flex h-[75px] items-center w-full bottom-0 left-0 bg-dark z-20'>
+      <div className='flex items-center h-full pl-3 gap-3 flex-1'>
+        <div className='w-[38px] h-[38px] lg:w-[45px] lg:h-[45px]'>
+          <img src={image ? `${image}` : 'https://f4.bcbits.com/img/a4139357031_10.jpg'} alt='' className='w-full h-full' />
         </div>
-        <div className='song-details'>
-          <h2 className='song-title'>{title}</h2>
-          <h4 className='more-song-details'>{`${artist} • ${albumName} • 2008`}</h4>
+        <div className='flex flex-col'>
+          <h2 className='text-sm lg:text-base text-nowrap'>{title}</h2>
+          <h4 className='text-sm lg:text-base text-nowrap'>{`${artist} • ${albumName}`}</h4>
         </div>
-        <div className="plus-button">
-          <FontAwesomeIcon icon={faPlus} className='icon' />
+        <div className='hidden md:flex items-center justify-center p-1 xl:p-[6px] border-2 rounded-full border-gray cursor-pointer'>
+          <FontAwesomeIcon icon={faPlus} className='text-sm lg:text-base' />
         </div>
       </div>
 
-      <div className="middle-control">
-        <div className="control-buttons">
-          <div className="prev-button" onClick={() => handlePrev(songList, currentSongId, setCurrentSongId, setIsPlayedId, dispatch)}>
-            <FontAwesomeIcon icon={faBackwardStep} className='icon' />
+      <div className='flex flex-col items-center justify-center h-full gap-2 flex-1'>
+        <div className='flex items-center justify-center gap-6'>
+          <div className='h-6 w-6 flex items-center justify-center cursor-pointer' onClick={() => handlePrev(songList, currentSongId, setCurrentSongId, setIsPlayedId, dispatch)}>
+            <FontAwesomeIcon icon={faBackwardStep} className='text-xl md:text-2xl' />
           </div>
           {isPlayedId
             ?
-            <div className="pause-button" onClick={() => handleStop(setIsPlayedId, dispatch)}>
-              <FontAwesomeIcon icon={faPause} className='icon' />
+            <div className='h-6 w-6 flex items-center justify-center cursor-pointer' onClick={() => handleStop(setIsPlayedId, dispatch)}>
+              <FontAwesomeIcon icon={faPause} className='text-[25px] md:text-[28px]' />
             </div>
             :
-            <div className="play-button" onClick={() => handlePlay(currentSongId, setCurrentSongId, setIsPlayedId, dispatch)}>
-              <FontAwesomeIcon icon={faPlay} className='icon' />
+            <div className='h-6 w-6 flex items-center justify-center cursor-pointer' onClick={() => handlePlay(currentSongId, setCurrentSongId, setIsPlayedId, dispatch)}>
+              <FontAwesomeIcon icon={faPlay} className='text-xl md:text-2xl' />
             </div>
           }
-          <div className="next-button" onClick={() => handleNext(songList, currentSongId, setCurrentSongId, setIsPlayedId, dispatch)}>
-            <FontAwesomeIcon icon={faForwardStep} className='icon' />
+          <div className='h-6 w-6 flex items-center justify-center cursor-pointer' onClick={() => handleNext(songList, currentSongId, setCurrentSongId, setIsPlayedId, dispatch)}>
+            <FontAwesomeIcon icon={faForwardStep} className='text-xl md:text-2xl' />
           </div>
         </div>
-        <div className="durations">
-          <h5>{`00.00 / ${duration}`}</h5>
+        <div className='flex items-center justify-center px-2'>
+          <h5 className='text-xs text-gray'>{`00.00 / ${duration}`}</h5>
         </div>
       </div>
 
-      <div className="right-control">
-        <div className="control-buttons">
-          <FontAwesomeIcon icon={faVolumeLow} className='icon' />
-          <FontAwesomeIcon icon={faRepeat} className='icon' />
-          <FontAwesomeIcon icon={faShuffle} className='icon' />
+      <div className='flex items-center justify-end h-full pr-3 flex-1'>
+        <div className='flex gap-6'>
+          <FontAwesomeIcon icon={faVolumeLow} className='text-xl' />
+          <FontAwesomeIcon icon={faRepeat} className='text-xl' />
+          <FontAwesomeIcon icon={faShuffle} className='text-xl' />
         </div>
       </div>
     </div >
